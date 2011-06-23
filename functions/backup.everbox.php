91afd310f43622a87f0e74adfc66859895651a80
likang <likang@kisbear.com>
2011-06-23 17:52:31 +0800

    back

--- functions/backup.everbox.php
+++ functions/backup.everbox.php
@@ -8,7 +8,43 @@ function hmbkp_save_to_everbox( $path ) {
   session_start();
   $sdid =  $_SESSION['sdid'];
   $token = $_SESSION['snda_token'];
+  $config = getSndaConfig();
+  $snda_oauth = new OauthSDK($config['appId'], $config['appSecret'], $config['redirectURI']);
+  $snda_oauth->setParam('oauth_token',$token);
+  $snda_oauth->setParam('sdid',$sdid);
+  $snda_oauth->setParam('method','sdo.everbox.fs.mkdir');
+  $snda_oauth->setParam('path','/home/wpbackup');
+  $result = $snda_oauth->request('GET');
+  //if the error is not dir already exist
+  if ($snda_oauth->getLastErrCode() &&
+    $snda_oauth->getLastErrorCode() != '-50000409') {
+    echo 'Error Code:', $snda_oauth->getLastErrCode(), '<br />';
+    echo 'Error Msg:', $snda_oauth->getLastErrMsg(), '<br />';
+    echo 'failed';
+    exit;
+  } 
+  //echo $path;
+  //echo json_encode($result);
+
   //print $path;
+}
+
+/**
+ * Ensure that there is a folder named 'wpbackup' in the root folder of everbox.
+ */
+function hmbkp_conform_everbox_dir( $snda_oauth ) {
+  $snda_oauth->setParam('method','sdo.everbox.fs.mkdir');
+  $snda_oauth->setParam('path','/home/wpbackup');
+  $result = $snda_oauth->request('GET');
+  //if the error is not dir already exist
+  if ($snda_oauth->getLastErrCode() &&
+    $snda_oauth->getLastErrorCode() != '-50000409') {
+    echo 'Error Code:', $snda_oauth->getLastErrCode(), '<br />';
+    echo 'Error Msg:', $snda_oauth->getLastErrMsg(), '<br />';
+    echo 'failed';
+    return False;
+  } 
+  return True;
 }
 
 /**
